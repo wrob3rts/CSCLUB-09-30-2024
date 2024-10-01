ce55d6e8ff1d6d8970088d3b2c4f406a1365bbf7
William Roberts <willenderroberts@gmail.com>
2024-10-01 12:30:08 -0400

    Innit Commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm> 
+
+int usaco2016JanQ2(const char* inputFileName, const char* outputFileName, int divisor) {
+
+
+
+    return 0;
+}
+
+int usaco2020DecQ2(){
+
+}
+
+
+int main() {
+
+    usaco2016JanQ2("div.in", "div.out", 7);
+
+	return 0;
+}
